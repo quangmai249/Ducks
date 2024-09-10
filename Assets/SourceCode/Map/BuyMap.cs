43555c322e10d7d6eac2b987a01808d39cd14f97
Mai Quang <quangmai249@gmail.com>
2024-09-10 11:33:51 +0700

    Text Coin in Menu Select Map

--- Assets/SourceCode/Map/BuyMap.cs
+++ Assets/SourceCode/Map/BuyMap.cs
@@ -7,29 +7,29 @@ using UnityEngine.UI;
 public class BuyMap : MonoBehaviour
 {
     public TextMeshProUGUI textMoney;
-    public TextMeshProUGUI textConfirm;
-    public TextMeshProUGUI textName;
-    public RawImage imgConfirm;
-    public MapSelection mapSelection;
+    //public TextMeshProUGUI textConfirm;
+    //public TextMeshProUGUI textName;
+    //public RawImage imgConfirm;
+    //public MapSelection mapSelection;
     public Money money;
     private void Start()
     {
         textMoney.text = money.GetMoney().ToString();
-        textName.text = mapSelection.name;
-        textConfirm.gameObject.SetActive(false);
-        imgConfirm.gameObject.SetActive(false);
+        //textName.text = mapSelection.name;
+        //textConfirm.gameObject.SetActive(false);
+        //imgConfirm.gameObject.SetActive(false);
     }
     public void ClickToConfirm()
     {
-        textConfirm.gameObject.SetActive(true);
-        textConfirm.text = $"{money.GetMoney() - Random.Range(0, 500)}";
+        //textConfirm.gameObject.SetActive(true);
+        //textConfirm.text = $"{money.GetMoney() - Random.Range(0, 500)}";
     }
     public void ClickToBuy()
     {
-        imgConfirm.gameObject.SetActive(true);
+        //imgConfirm.gameObject.SetActive(true);
     }
     public void CancelBuying()
     {
-        imgConfirm.gameObject.SetActive(false);
+        //imgConfirm.gameObject.SetActive(false);
     }
 }
