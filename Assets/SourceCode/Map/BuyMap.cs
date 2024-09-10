d7a627ee00433f767ca48324fab0f9cb89e1cdd9
Mai Quang <quangmai249@gmail.com>
2024-09-10 09:55:09 +0700

    Remove Buy Map

--- Assets/SourceCode/Map/BuyMap.cs
+++ Assets/SourceCode/Map/BuyMap.cs
@@ -8,18 +8,21 @@ public class BuyMap : MonoBehaviour
 {
     public TextMeshProUGUI textMoney;
     public TextMeshProUGUI textConfirm;
+    public TextMeshProUGUI textName;
     public RawImage imgConfirm;
+    public MapSelection mapSelection;
     public Money money;
     private void Start()
     {
         textMoney.text = money.GetMoney().ToString();
+        textName.text = mapSelection.name;
         textConfirm.gameObject.SetActive(false);
         imgConfirm.gameObject.SetActive(false);
     }
     public void ClickToConfirm()
     {
         textConfirm.gameObject.SetActive(true);
-        textConfirm.text = $"{money.GetMoney() - 100}";
+        textConfirm.text = $"{money.GetMoney() - Random.Range(0, 500)}";
     }
     public void ClickToBuy()
     {
